86ab012420face9e83dd904d1fb3765109c1b24c
zamhaq <haque864@gmail.com>
2017-07-12 19:00:42 +0530

    Add delete script

--- playground/find.js
+++ playground/find.js
@@ -1,21 +1,21 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient,ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
   if(err)
     return console.log('Connect failed!');
     console.log('Successfully connected to database!');
-    //   text: 'Study Mean stack'
     // db.collection('todos').insertOne({
+    //   text: 'Study Mean stack'
     // },(err,res) => {
     //   if(err)
     //   return console.log('Failed to save!');
     //   console.log(JSON.stringify(res.ops,undefined,2));
     // });
-    db.collection('user').find({
-      name: "Shantam"
-    }).toArray().then((doc) => {
-      console.log(doc);
-    }, (err) => {
+    db.collection('user').findOneAndDelete({
+      _id: new ObjectID("59658fae12e36d19ad333823")
+    }).then((res) => {
+      console.log(res);
+    },(err) => {
       console.log(err);
     });
   db.close();
